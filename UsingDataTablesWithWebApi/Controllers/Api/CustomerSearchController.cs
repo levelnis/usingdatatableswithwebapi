32e86e5fcac66e001b9ed6324d7ead1db5698cb5
Simon Rigby <simon.rigby@levelnis.co.uk>
2016-11-26 17:39:36 +0000

    Added filtering to results

--- UsingDataTablesWithWebApi/Controllers/Api/CustomerSearchController.cs
+++ UsingDataTablesWithWebApi/Controllers/Api/CustomerSearchController.cs
@@ -1,23 +1,34 @@
 ﻿namespace Levelnis.Learning.UsingDataTablesWithWebApi.Controllers.Api
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Http;
+    using Details;
+    using Infrastructure;
     using Newtonsoft.Json;
     using Requests;
     using Responses;
 
-    public class CustomerSearchController : ApiController
+    public class CustomerSearchController : SearchController
     {
         public IHttpActionResult Post(SearchRequest request)
         {
             var allCustomers = JsonConvert.DeserializeObject<CustomerData>(CustomerData.DataSource);
+            var response = WrapSearch(allCustomers.Data, request);
+            return Ok(response);
+        }
+
+        private static CustomerSearchResponse WrapSearch(ICollection<CustomerSearchDetail> details, SearchRequest request)
+        {
+            var results = ApiHelper.FilterCustomers(details, request.Search.Value).ToList();
             var response = new CustomerSearchResponse
             {
-                Data = allCustomers.Data,
+                Data = PageResults(results, request),
                 Draw = request.Draw,
-                RecordsFiltered = allCustomers.Data.Count,
-                RecordsTotal = allCustomers.Data.Count
+                RecordsFiltered = results.Count,
+                RecordsTotal = details.Count
             };
-            return Ok(response);
+            return response;
         }
     }
 }
